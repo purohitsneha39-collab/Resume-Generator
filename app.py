5491dd5f67b2cf5bd11734fec7898b833af2e4ad
purohitsneha39-collab <purohitsneha39@gmail.com>
2026-07-31 16:22:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ if all(ALL_API) and user_query:
         data = f"https://image.pollinations.ai/{user_query}"
         import requests as r
         img_data=r.get(data)
-        time.sleep(3)
+        #time.sleep(3)
         st.image(data)
         
 
